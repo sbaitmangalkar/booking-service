886198cfb153aac7856d85444bba5d2cb025ded6
Shyam <catch.shyambaitmangalkar@gmail.com>
2018-01-25 18:27:24 +0530

    Modifying BookingServiceApplication

--- src/main/java/com/booking/bookingservice/BookingServiceApplication.java
+++ src/main/java/com/booking/bookingservice/BookingServiceApplication.java
@@ -14,7 +14,9 @@ public class BookingServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookingServiceApplication.class, args);
 	}
-	
+	/*
+	 * Command line runner
+	 */
 	@Bean
 	CommandLineRunner demoRunner(BookingRepository bookingRepo) {
 		return args -> {
